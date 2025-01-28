86a3c1491b88731663943606157d746b0c84b1bd
eddguy <ecubasmarine2019@fau.edu>
2025-01-27 20:22:25 -0500

    Basic layout of Workspace page

--- frontend/pat/src/Workspace/Workspace.js
+++ frontend/pat/src/Workspace/Workspace.js
@@ -1,6 +1,24 @@
+import Display from "./Display";
+import Pat from "./Pat";
+import Editor from "./Editor";
+
+
 const Workspace = () => {
-    return ( 
-        <p>add everything</p>
+    return (
+        <div class="workspace_container" >
+            <div class="pat_container" id="container">
+                <Display />
+                <Pat />
+            </div>
+
+            <div class="editor_container" id="container">
+                <Editor />
+
+            </div>
+
+        </div> 
+        
+
      );
 }
  
