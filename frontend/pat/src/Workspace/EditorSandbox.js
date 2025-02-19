c83cb4f68a78ca111b575ab3ac293521238f821f
eddguy <ecubasmarine2019@fau.edu>
2025-02-19 00:50:48 -0500

    Added custom editor theme (pat.css)

--- frontend/pat/src/Workspace/EditorSandbox.js
+++ frontend/pat/src/Workspace/EditorSandbox.js
@@ -1,10 +1,12 @@
 import AceEditor from 'react-ace';
 import 'ace-builds/src-noconflict/mode-python';
-import 'ace-builds/src-noconflict/theme-monokai';
+import 'ace-builds/src-noconflict/theme-pat';
+import '../pat.css'
+
 
 const EditorSandbox = ({ 
     mode = 'python', 
-    theme = 'monokai', 
+    theme = 'pat', 
     width = '100%', 
     height = '100%', 
     onChange 
@@ -12,26 +14,25 @@ const EditorSandbox = ({
     return ( 
         <div className="editor_sandbox">
             <AceEditor 
-    mode={mode}
-    theme={theme}
-    width={width}
-    height={height}
-    onChange={onChange}
-    tabSize={4} // Set tab size to 4 spaces
-    useSoftTabs={true} // Use spaces instead of tabs
-    showPrintMargin={false} // Optionally disable print margin
-    setOptions={{
-        enableBasicAutocompletion: true,
-        enableLiveAutocompletion: true,
-        enableSnippets: true,
-        showLineNumbers: true,
-        tabSize: 4,
-        useSoftTabs: true,
-        wrap: true, // Auto-wrap long lines
-    }}
-    editorProps={{ $blockScrolling: true }}
-/>
-
+                mode={mode}
+                theme={theme}
+                fontSize={18}
+                width={width}
+                height={height}
+                onChange={onChange}
+                tabSize={4}
+                useSoftTabs={true}
+                showPrintMargin={false}
+                setOptions={{
+                    enableBasicAutocompletion: true,
+                    enableLiveAutocompletion: true,
+                    enableSnippets: true,
+                    showLineNumbers: true,
+                    tabSize: 4,
+                    wrap: true, // Auto-wrap long lines
+                }}
+                editorProps={{ $blockScrolling: true }}
+            />
         </div>
     );
 }
